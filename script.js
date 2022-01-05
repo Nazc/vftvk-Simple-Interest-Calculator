59f9c2deac91dc2b1a735766ba58e8e1847cb11d
Alexander Zermeño <alexander.zermeno@uabc.edu.mx>
2022-01-05 01:54:00 +0000

    Added alert message

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 
+    if (principal==0)
+    {
+        alert("Enter a positive number");
+    }
+
     document.getElementById("result").innerHTML="\<br\>If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>";
 }
 
